caedfbf12d33d608eee07d9a2b97fd36f1f4d8e5
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-03-01 18:29:53 -0500

    spring book structure

---
+++ src/main/java/com/bluni/bluni/BluniApplication.java
@@ -0,0 +1,13 @@
+package com.bluni.bluni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluniApplication.class, args);
+	}
+
+}

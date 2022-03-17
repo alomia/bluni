8793d8fc2d8c027b5cd0de8b9016dd80ef8b816a
jdbalantar <jdbalantar@gmail.com>
2022-03-17 18:22:13 -0500

    Se arregla la excepción en el login y se implementan roles

--- src/main/java/com/bluni/bluni/BluniApplication.java
+++ src/main/java/com/bluni/bluni/BluniApplication.java
@@ -7,19 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class BluniApplication implements CommandLineRunner {
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
-
+public class BluniApplication  {
     public static void main(String[] args) {
         SpringApplication.run(BluniApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        String pass1 = "user";
-        String pass2 = "admin";
-        System.out.println(passwordEncoder.encode(pass1));
-        System.out.println(passwordEncoder.encode(pass2));
     }
 }
--- src/main/java/com/bluni/bluni/WebConfig.java
+++ src/main/java/com/bluni/bluni/WebConfig.java
@@ -3,6 +3,7 @@ package com.bluni.bluni;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,6 +16,21 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/index", "/home", "/", "/css/**", "/js/**", "img/**").permitAll()
+                .antMatchers("/views/computers/").hasAnyRole("USER")
+                .antMatchers("/views/computers/create").hasAnyRole("ADMIN")
+                .antMatchers("/views/computers/save").hasAnyRole("ADMIN")
+                .antMatchers("/views/computers/edit/**").hasAnyRole("ADMIN")
+                .antMatchers("/views/computers/delete/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
+    }
 
     @Autowired
     public void configureSecurityGlobal(AuthenticationManagerBuilder builder) throws Exception{

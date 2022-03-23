d73591a3c1ca177e280bda3f55048d686e3db55f
F-balanta <jf.balanta@hotmail.com>
2022-03-23 15:13:55 -0500

    Se añadieron estilos para el header

--- src/main/resources/static/js/sidebar.js
+++ src/main/resources/static/js/sidebar.js
@@ -1,22 +1,17 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
-
+let mheader = document.querySelector("header");
 closeBtn.addEventListener("click", () => {
     sidebar.classList.toggle("open");
     menuBtnChange();//calling the function(optional)
 });
 
-searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
-    sidebar.classList.toggle("open");
-    menuBtnChange(); //calling the function(optional)
-});
-
-
 function menuBtnChange() {
     if (sidebar.classList.contains("open")) {
         closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+        mheader.classList.replace('header', 'barra_header');
     } else {
+        mheader.classList.replace('barra_header', 'header');
         closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
     }
 }

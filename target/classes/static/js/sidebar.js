ca574dc6686456ddd0381948dd67aff226108b49
F-balanta <jf.balanta@hotmail.com>
2022-03-24 18:10:10 -0500

    Se mejora el aspecto visual

--- target/classes/static/js/sidebar.js
+++ target/classes/static/js/sidebar.js
@@ -20,3 +20,19 @@ function menuBtnChange() {
 function limpiarCampos() {
     window.location='/views/computers/';
 }
+
+$(document).ready(function() {
+    $(".contactclass").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#D_contact").offset().top
+        }, 500);
+    });
+});
+
+$(document).ready(function() {
+    $(".aboutclass").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#aboutid").offset().top
+        }, 1);
+    });
+});

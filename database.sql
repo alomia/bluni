901961c6073483737c5f00612b7bc60f9b161f4e
jdbalantar <jdbalantar@gmail.com>
2022-03-23 22:59:00 -0500

    Update db

--- database.sql
+++ database.sql
@@ -1,37 +1,44 @@
-CREATE DATABASE  IF NOT EXISTS `bluni` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `bluni`;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-03-2022 a las 04:57:53
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 7.4.27
 
--- CREATE USER
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE USER IF NOT EXISTS 'bluni'@'localhost'  IDENTIFIED BY 'bluni';
-GRANT ALL PRIVILEGES ON *.* TO 'bluni'@'localhost';
-FLUSH PRIVILEGES;
 
--- CREATE TABLE users
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `users` (
-                         `id` int(11) NOT NULL AUTO_INCREMENT,
-                         `username` varchar(45) NOT NULL,
-                         `password` varchar(60) NOT NULL,
-                         `enabled` tinyint(1) NOT NULL DEFAULT '1',
-                         PRIMARY KEY (`id`),
-                         UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB
+--
+-- Base de datos: `bluni`
+--
 
--- CREATE TABLE roles
-CREATE TABLE `roles` (
-                         `id` int(11) NOT NULL AUTO_INCREMENT,
-                         `user_id` int(11) NOT NULL,
-                         `rol` varchar(45) NOT NULL,
-                         PRIMARY KEY (`id`),
-                         UNIQUE KEY `user_id_rol_unique` (`user_id`,`rol`) /*!80000 INVISIBLE */,
-                         CONSTRAINT `fk_rol_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB
+--
+-- Volcado de datos para la tabla `roles`
+--
 
--- INSERTS
+INSERT INTO `roles` (`id`, `user_id`, `rol`) VALUES
+                                                 (1, 1, 'ROLE_USER'),
+                                                 (3, 2, 'ROLE_ADMIN'),
+                                                 (2, 2, 'ROLE_USER');
 
-insert into users(username, password, enabled) values('user','',1);
-insert into users(username, password, enabled) values('admin','',1);
-insert into roles(user_id, rol) values(1, 'ROLE_USER');
-insert into roles(user_id, rol) values(2, 'ROLE_USER');
-insert into roles(user_id, rol) values(2, 'ROLE_ADMIN');
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `enabled`) VALUES
+                                                                  (1, 'user', '$2a$10$KKN/gjZ/eKQTvlkUrxTBYOUdqjHn/if2lx585Hpb94jXDQFfTmkVm', 1),
+                                                                  (2, 'admin', '$2a$10$ftbHEG1WT0bTSrRMxVI6tOKvT0WakVPiaUTbVXGoDsJn8hFqgwqlO', 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
